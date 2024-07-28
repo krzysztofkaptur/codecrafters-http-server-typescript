ece929f15ef540142695724b8f9b75420e807b51
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-07-28 15:04:58 +0200

    Respond with body

--- app/main.ts
+++ app/main.ts
@@ -7,6 +7,13 @@ const server = net.createServer(socket => {
 
     if (path === '/') {
       socket.write('HTTP/1.1 200 OK\r\n\r\n')
+    } else if (path.includes('echo')) {
+      const response = path.split('/')
+      const msg = response[response.length - 1]
+
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n${msg}`
+      )
     } else {
       socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
     }
