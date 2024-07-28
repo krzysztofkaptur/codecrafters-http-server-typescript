1f3d3c3746e9843a47bf9fb60cf0ee7428ff9edc
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-07-28 11:31:57 +0200

    Extract URL path

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,16 @@
 import * as net from 'net'
 
 const server = net.createServer(socket => {
-  socket.write('HTTP/1.1 200 OK\r\n\r\n')
+  socket.on('data', data => {
+    const dataStr = data.toString()
+    const path = dataStr.split(' ')[1]
+
+    if (path === '/') {
+      socket.write('HTTP/1.1 200 OK\r\n\r\n')
+    } else {
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+    }
+  })
 
   socket.on('close', () => {
     console.log('close')
