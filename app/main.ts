053db81355c7a882bf62646e3e4ed793f760ae2a
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-07-28 22:06:43 +0200

    Return a file

--- app/main.ts
+++ app/main.ts
@@ -1,20 +1,22 @@
 import * as net from 'net'
+import fs from 'node:fs'
+import path from 'path'
 
 const server = net.createServer(socket => {
   socket.on('data', data => {
     const dataStr = data.toString()
-    const path = dataStr.split(' ')[1]
+    const pathStr = dataStr.split(' ')[1]
 
-    if (path === '/') {
+    if (pathStr === '/') {
       socket.write('HTTP/1.1 200 OK\r\n\r\n')
-    } else if (path.includes('echo')) {
-      const response = path.split('/')
+    } else if (pathStr.includes('echo')) {
+      const response = pathStr.split('/')
       const msg = response[response.length - 1]
 
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${msg.length}\r\n\r\n${msg}`
       )
-    } else if (path.includes('user-agent')) {
+    } else if (pathStr.includes('user-agent')) {
       const userAgent = dataStr
         .split('User-Agent:')[1]
         .replace(/\r/g, '')
@@ -26,6 +28,18 @@ const server = net.createServer(socket => {
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
       )
+    } else if (pathStr.includes('files')) {
+      const fileName = pathStr.split('files/')[1]
+      const tmpFolder = path.join(__dirname, 'tmp')
+
+      if (fs.existsSync(`${tmpFolder}/${fileName}`)) {
+        const fileContent = fs.readFileSync(`${tmpFolder}/${fileName}`, 'utf-8')
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`
+        )
+      } else {
+        socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+      }
     } else {
       socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
     }
