65db17f953ad156f55cb5443d105ac12da89bab2
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-07-29 11:10:28 +0200

    Read request body

--- app/main.ts
+++ app/main.ts
@@ -6,42 +6,58 @@ const server = net.createServer(socket => {
   socket.on('data', data => {
     const dataStr = data.toString()
     const pathStr = dataStr.split(' ')[1]
+    const method = dataStr.split(' ')[0]
 
-    if (pathStr === '/') {
-      socket.write('HTTP/1.1 200 OK\r\n\r\n')
-    } else if (pathStr.includes('echo')) {
-      const response = pathStr.split('/')
-      const msg = response[response.length - 1]
+    if (method === 'GET') {
+      if (pathStr === '/') {
+        socket.write('HTTP/1.1 200 OK\r\n\r\n')
+      } else if (pathStr.includes('echo')) {
+        const response = pathStr.split('/')
+        const msg = response[response.length - 1]
 
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${msg.length}\r\n\r\n${msg}`
-      )
-    } else if (pathStr.includes('user-agent')) {
-      const userAgent = dataStr
-        .split('User-Agent:')[1]
-        .replace(/\r/g, '')
-        .replace(/\n/g, '')
-        .trim()
-
-      console.log(userAgent)
-
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
-      )
-    } else if (pathStr.includes('files')) {
-      const fileName = pathStr.split('files/')[1]
-      const tmpFolder = process.argv[3]
-
-      if (fs.existsSync(`${tmpFolder}/${fileName}`)) {
-        const fileContent = fs.readFileSync(`${tmpFolder}/${fileName}`, 'utf-8')
         socket.write(
-          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`
+          `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${msg.length}\r\n\r\n${msg}`
         )
+      } else if (pathStr.includes('user-agent')) {
+        const userAgent = dataStr
+          .split('User-Agent:')[1]
+          .replace(/\r/g, '')
+          .replace(/\n/g, '')
+          .trim()
+
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+        )
+      } else if (pathStr.includes('files')) {
+        const fileName = pathStr.split('files/')[1]
+        const tmpFolder = process.argv[3]
+
+        if (fs.existsSync(`${tmpFolder}/${fileName}`)) {
+          const fileContent = fs.readFileSync(
+            `${tmpFolder}/${fileName}`,
+            'utf-8'
+          )
+          socket.write(
+            `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`
+          )
+        } else {
+          socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+        }
       } else {
         socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
       }
-    } else {
-      socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+    } else if (method === 'POST') {
+      if (pathStr.includes('files')) {
+        const fileName = pathStr.split('files/')[1]
+        const tmpFolder = process.argv[3]
+
+        const bodyArr = dataStr.split('\r\n\r\n')
+        const content = bodyArr[bodyArr.length - 1]
+
+        fs.writeFileSync(`${tmpFolder}/${fileName}`, content)
+
+        socket.write('HTTP/1.1 201 Created\r\n\r\n')
+      }
     }
   })
 
