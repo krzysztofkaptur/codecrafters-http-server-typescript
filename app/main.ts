da506d386f1329fbee1b3930b7c196a12687cda4
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-07-30 10:08:57 +0200

    revert

--- app/main.ts
+++ app/main.ts
@@ -2,35 +2,22 @@ import * as net from 'net'
 import fs from 'node:fs'
 import zlib from 'zlib'
 
-function deconstructData(data: Buffer) {
-  const dataStr = data.toString()
-  const dataArr = dataStr.split('\r\n')
-  
-  const method = dataArr.find(item => item.includes('HTTP'))?.split(' ')[0]
-  const path = dataArr.find(item => item.includes('HTTP'))?.split(' ')[1]
-  const body = dataArr[dataArr.length - 1]
-  const encoding = dataArr.find(item => item.includes('Accept-Encoding'))?.split(': ')?.[1]
-  const userAgent = dataArr.find(item => item.includes('User-Agent'))?.split(': ')?.[1]
-
-  return {
-    method,
-    path,
-    body,
-    encoding,
-    userAgent
-  }
-}
-
 const server = net.createServer(socket => {
   socket.on('data', data => {
-    const { method, path, body, encoding, userAgent } = deconstructData(data)
-    
+    const dataStr = data.toString()
+    const pathStr = dataStr.split(' ')[1]
+    const method = dataStr.split(' ')[0]
+    const acceptEncoding = dataStr.split('\r\n')?.[dataStr.split('\r\n')?.findIndex(item => item.includes("Accept-Encoding"))]?.split(": ")?.[1] || ''
+
     if (method === 'GET') {
-      if (path === '/') {
+      if (pathStr === '/') {
         socket.write('HTTP/1.1 200 OK\r\n\r\n')
-      } else if (path?.includes('echo')) {
-        if(encoding?.includes('gzip')) {
-          const buf = Buffer.from(body, 'utf-8')
+      } else if (pathStr.includes('echo')) {
+        const response = pathStr.split('/')
+        const msg = response[response.length - 1]
+
+        if(acceptEncoding.includes('gzip')) {
+          const buf = Buffer.from(msg, 'utf-8')
           const gzipMsg = zlib.gzipSync(buf)
           
           socket.write(
@@ -39,16 +26,21 @@ const server = net.createServer(socket => {
           socket.write(gzipMsg)
         } else {
           socket.write(
-            `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${body.length}\r\n\r\n${body}`
+            `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${msg.length}\r\n\r\n${msg}`
           )
         }
 
-      } else if (path?.includes('user-agent')) {
+      } else if (pathStr.includes('user-agent')) {
+        const userAgent = dataStr
+          .split('User-Agent:')[1]
+          .replace(/\r\n/g, '')
+          .trim()
+
         socket.write(
-          `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent?.length}\r\n\r\n${userAgent}`
+          `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
         )
-      } else if (path?.includes('files')) {
-        const fileName = path.split('files/')[1]
+      } else if (pathStr.includes('files')) {
+        const fileName = pathStr.split('files/')[1]
         const tmpFolder = process.argv[3]
 
         if (fs.existsSync(`${tmpFolder}/${fileName}`)) {
@@ -62,15 +54,20 @@ const server = net.createServer(socket => {
         } else {
           socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
         }
+      } else if(pathStr.includes("echo")) {
+        console.log("dupa")
       } else {
         socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
       }
     } else if (method === 'POST') {
-      if (path?.includes('files')) {
-        const fileName = path.split('files/')[1]
+      if (pathStr.includes('files')) {
+        const fileName = pathStr.split('files/')[1]
         const tmpFolder = process.argv[3]
 
-        fs.writeFileSync(`${tmpFolder}/${fileName}`, body)
+        const bodyArr = dataStr.split('\r\n\r\n')
+        const content = bodyArr[bodyArr.length - 1]
+
+        fs.writeFileSync(`${tmpFolder}/${fileName}`, content)
 
         socket.write('HTTP/1.1 201 Created\r\n\r\n')
       }
